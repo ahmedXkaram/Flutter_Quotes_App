c4f11ecaf29ae48d1fb0d66857ceb4dd17f81800
ahmedXkaram <karam7423@gmail.com>
2021-11-04 21:10:12 +0200

    Uploading Flutter Quotes App

---
+++ lib/quotes.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/material.dart';
+
+class Wisdom extends StatefulWidget {
+  @override
+  _WisdomState createState() => _WisdomState();
+}
+
+class _WisdomState extends State<Wisdom> {
+  int index = 0;
+
+  List quotes = [
+    "Stay Hungry. Stay Foolish.",
+    "Good Artists Copy, Great Artists Steal.",
+    "Argue with idiots, and you become an idiot.",
+    "Be yourself; everyone else is already taken.",
+    "Simplicity is the ultimate sophistication."
+  ];
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Quotes'),
+      ),
+      body: Container(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Container(
+              width: 350.0,
+              height: 320.0,
+              margin: EdgeInsets.symmetric(vertical: 20.0),
+              decoration: BoxDecoration(
+                color: Colors.greenAccent[200],
+                borderRadius: BorderRadius.circular(15.0),
+              ),
+              child: Center(
+                child: Text(
+                  quotes[index % quotes.length],
+                  style: TextStyle(
+                    fontStyle: FontStyle.italic,
+                    fontSize: 20.0,
+                  ),
+                ),
+              ),
+            ),
+            Divider(
+              thickness: 2.3,
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: TextButton.icon(
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.greenAccent[100],
+                ),
+                icon: Icon(Icons.wb_sunny),
+                label: Text('Inspire me!'),
+                onPressed: showQuote,
+              ),
+            ),
+            Spacer()
+          ],
+        ),
+      ),
+    );
+  }
+
+  void showQuote() {
+    setState(() {
+      index++;
+    });
+  }
+}
